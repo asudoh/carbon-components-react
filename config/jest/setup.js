ef25c83e99716825b86fad55cba960028bfd8120
Akira Sudoh <asudoh@jp.ibm.com>
2017-11-22 14:20:48 +0900

    test: support React16 for test
    Fixes #280.

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -6,13 +6,14 @@ jest.unmock('object-assign');
 
 require('../polyfills');
 
-const enzyme = require.requireActual('enzyme');
-const Adapter = require.requireActual('enzyme-adapter-react-15');
-
-// Configure enzyme to work with React 15 for now
-enzyme.configure({ adapter: new Adapter() });
-
 // Starting with React 16, we'll have to polyfill this in test environments.
+// Defining this earlier than loading Enzyme to avoid "React depends on requestAnimationFrame" warning.
 global.requestAnimationFrame = function(callback) {
   callback();
 };
+
+const enzyme = require.requireActual('enzyme');
+const Adapter = require.requireActual('enzyme-adapter-react-16');
+
+// Configure enzyme to work with React 15 for now
+enzyme.configure({ adapter: new Adapter() });
